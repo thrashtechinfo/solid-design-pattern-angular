cafb76dc4fca255ab73045f3cbe74b3c03360ec3
Muhammad Awais <awaisshaikh94@gmail.com>
2024-10-02 20:54:55 +0500

    I: Interface Segregation Principle - done

--- src/app/interface-segregation-principle/with-isp/with-isp.component.ts
+++ src/app/interface-segregation-principle/with-isp/with-isp.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CreditCardService } from './credit-card.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-with-isp',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './with-isp.component.html',
   styleUrl: './with-isp.component.css'
 })
 export class WithIspComponent {
+  paymentStatus: string = '';
+  payment = signal<number>(0);
 
+  constructor(
+    private paymentService: CreditCardService
+  ) { }
+
+  processPayment(amount: number) {
+    this.paymentService.processPayment(amount);
+  }
+
+  refundPayment(amount: number) {
+    this.paymentService.refundPayment(amount);
+  }
 }
