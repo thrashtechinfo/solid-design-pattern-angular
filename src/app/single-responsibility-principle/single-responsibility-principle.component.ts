f77a12505f52eb023cff25d0a36b1beeb38f1672
Muhammad Awais <awaisshaikh94@gmail.com>
2024-10-02 17:38:17 +0500

    without-srp done

--- src/app/single-responsibility-principle/single-responsibility-principle.component.ts
+++ src/app/single-responsibility-principle/single-responsibility-principle.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { WithSrpComponent } from './with-srp/with-srp.component';
+import { WithoutSrpComponent } from './without-srp/without-srp.component';
 
 @Component({
   selector: 'app-single-responsibility-principle',
   standalone: true,
-  imports: [],
+  imports: [WithSrpComponent, WithoutSrpComponent],
   templateUrl: './single-responsibility-principle.component.html',
   styleUrl: './single-responsibility-principle.component.css'
 })
--- src/app/single-responsibility-principle/without-srp/without-srp.component.ts
+++ src/app/single-responsibility-principle/without-srp/without-srp.component.ts
@@ -8,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrl: './without-srp.component.css'
 })
 export class WithoutSrpComponent {
+  selectedProgram = '';
 
+  courses = [
+    { name: 'Introduction to AI', program: 'Computer Science' },
+    { name: 'Advanced Calculus', program: 'Mathematics' },
+    { name: 'Digital Marketing', program: 'Business' },
+  ];
+
+  get filteredCourses() {
+    if (!this.selectedProgram) {
+      return this.courses;
+    }
+
+    return this.courses.filter(course => course.program === this.selectedProgram);
+  }
+
+  onProgramChange(event: Event): void {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    this.selectedProgram = selectedValue;
+  }
 }
